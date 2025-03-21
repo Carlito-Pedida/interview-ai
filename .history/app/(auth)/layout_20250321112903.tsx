00407f0ffaf049b2efa3994db65291ccb8daafaa
Carlito Pedida Jr <carlped417@gmail.com>
2025-03-21 15:03:19 -0500

    history folder deleted

--- .history/app/(auth)/layout_20250321112903.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactNode } from "react";
-
-const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div>AuthLayout</div>;
-};
-
-export default AuthLayout;
--- .history/app/(auth)/sign-in/page_20250321113930.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignIn = () => {
-  return <div>Sign In Here!</div>;
-};
-
-export default SignIn;
