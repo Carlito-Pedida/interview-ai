4ecc153aaf91ac42646eadfd11acbdbf26cae9cd
Carlito Pedida Jr <carlped417@gmail.com>
2025-03-22 19:30:23 -0500

    authentication actions including sign in, sign up, session storing. AuthForm onSubmit function, .env files update. Current user authentication. page redirections.

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,19 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCagu3iT6tGKxQQGY4Gd5iRhFknRZ6qzeM",
-  authDomain: "interview-ai-f0572.firebaseapp.com",
-  projectId: "interview-ai-f0572",
-  storageBucket: "interview-ai-f0572.firebasestorage.app",
-  messagingSenderId: "12005480405",
-  appId: "1:12005480405:web:3841db77c967f39af4cf5c",
-  measurementId: "G-NG4B2BYB0J"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
