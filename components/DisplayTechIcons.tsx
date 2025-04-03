6902583c8ecb907e27391a5b8e3de73f88e5f9ea
Carlito Pedida Jr <carlped417@gmail.com>
2025-04-03 18:33:49 -0500

    interview format

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -10,7 +10,7 @@ const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
         <div
           key={tech}
           className={cn(
-            "relative group bg-dark-300 rounded-full p-2 flex-center",
+            "relative group bg-dark-300 rounded-full p-5 flex-center",
             index >= 1 && "-ml-3"
           )}
         >
@@ -20,7 +20,7 @@ const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
             alt={tech}
             width={100}
             height={100}
-            className="size-5"
+            className="size-10"
           />
         </div>
       ))}
